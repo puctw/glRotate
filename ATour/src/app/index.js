b60fdadedf1add1f07c69b577b0bfdd264add26e
HenriqueJLM <82334391+HenriqueJLM@users.noreply.github.com>
2024-04-21 02:54:52 +0100

    Add route

--- ATour/src/app/index.js
+++ ATour/src/app/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { View, Text, Pressable, Image } from 'react-native';
 import { Link, useRouter } from 'expo-router';
-import HomeScreenStyle from './homeStyles.js'; 
+import HomeScreenStyle from './homeStyles.js';
 
 const Home = () => {
-    const navigation = useRouter();
-    return (
-        <View style={HomeScreenStyle.container}>
-            <Image
-                source={require('../../assets/logo.png')} 
-                style={HomeScreenStyle.logo}
-                resizeMode="contain"
-            />
-            <Text style={HomeScreenStyle.text}>ATour</Text>
-            <Link push href='./map' asChild>
-                <Pressable style={HomeScreenStyle.startButton}>
-                    <Text style={HomeScreenStyle.buttonText}>Click to Start</Text>  
-                </Pressable>
-            </Link>
-            <Pressable style={HomeScreenStyle.createAccountButton} onPress={() => navigation.push('register')}>
-                <Text style={HomeScreenStyle.createAccountButtonText}>Criar conta</Text>
-            </Pressable>
-        </View>
-    );
+	const navigation = useRouter();
+	return (
+		<View style={HomeScreenStyle.container}>
+			<Image
+				source={require('../../assets/logo.png')}
+				style={HomeScreenStyle.logo}
+				resizeMode='contain'
+			/>
+			<Text style={HomeScreenStyle.text}>ATour</Text>
+			<Pressable
+				style={HomeScreenStyle.startButton}
+				onPress={() => navigation.push('/map')}
+			>
+				<Text style={HomeScreenStyle.buttonText}>Click to Start</Text>
+			</Pressable>
+			<Pressable
+				style={HomeScreenStyle.createAccountButton}
+				onPress={() => navigation.push('/register')}
+			>
+				<Text style={HomeScreenStyle.createAccountButtonText}>Criar conta</Text>
+			</Pressable>
+		</View>
+	);
 };
 
 export default Home;
